57c50def47d65dfb1054b4393496f264d1620d51
Kyle Lackinger <kbl40@georgetown.edu>
2022-05-13 23:35:45 -0400

    commented code and cleaned up

---
+++ README.md
@@ -0,0 +1,8 @@
+## Fibonacci Number Calculator
+This is a quick program from the Rust book that programatically prints the lyrics of the well-known carol, 12 Days of Christmas.
+
+From the terminal while in the root directory run:
+
+`cargo run` 
+
+This will print the lyrics out to the terminal.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::io;
 
 fn main() {
+    // Constants to set first two Fibonacci numbers
     const NUMBER_ZERO: u32 = 0;
     const NUMBER_ONE: u32 = 1;
 
+    // User input prompt
     println!("Enter the Fibonacci number you want to calculate.");
 
     let mut number = String::new();
@@ -12,11 +14,9 @@ fn main() {
         .read_line(&mut number)
         .expect("Please enter a number");
 
+    // Assign user entered number to variable
     let number: u64 = number.trim().parse().expect("Expected a number");
 
-    println!("The number is: {}", number);
-
-    // make a summing function
     let mut lead = NUMBER_ONE;
     let mut lag = NUMBER_ZERO;
     let mut sum = lag + lead;
@@ -27,13 +27,13 @@ fn main() {
         sum = 1;
         println!("The {} Fibonacci number is: {}", number, sum);
     } else {
+        // Generate Fibonacci numbers - following number in sequence is the sum of the two proceeding numbers
         for i in 3..number {
             lag = lead;
             lead = sum;
             sum = lead + lag;
-            //println!("The sum is: {}", sum);
     }
 
-    println!("The {}th Fibonacci number is: {}", number, sum)
+    println!("The {}th Fibonacci number is: {}", number, sum);
     }
 }
